53fbc507d2cc8f28c3e7be037e82293f8ca9a8c7
maxeusz <bartosz.mazur39@gmail.com>
2021-08-23 03:33:17 +0200

    stworzenie skryptu i pliku html

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 12;
+let secondNumber = 7;
+function funkcja(fNumber, sNumber){
+    let suma = fNumber + sNumber;
+    return (suma);
+}
+let suma = funkcja(firstNumber, secondNumber);
+alert(suma);
